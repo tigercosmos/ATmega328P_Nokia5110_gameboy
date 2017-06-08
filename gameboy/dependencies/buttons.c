21b80fddd467debe0e4f0d7ce9ded88dfdb0e1e1
tigercosmos <b04209032@ntu.edu.tw>
2017-06-08 22:30:36 +0800

    use button clicks as random seed

--- gameboy/dependencies/buttons.c
+++ gameboy/dependencies/buttons.c
@@ -1,29 +1,37 @@
+unsigned BTN_CLICKS = 0;
+
 unsigned char isBtnUp()
 {
+    BTN_CLICKS += 1;
     return BTN & (1 << BTN_UP);
 }
 
 unsigned char isBtnRight()
 {
+    BTN_CLICKS += 1;
     return BTN & (1 << BTN_RIGHT);
 }
 
 unsigned char isBtnLeft()
 {
+    BTN_CLICKS += 1;
     return BTN & (1 << BTN_LEFT);
 }
 
 unsigned char isBtnDown()
 {
+    BTN_CLICKS += 1;
     return BTN & (1 << BTN_DOWN);
 }
 
 unsigned char isBtnFire()
 {
+    BTN_CLICKS += 1;
     return BTN & (1 << BTN_FIRE);
 }
 
 unsigned char isBtnCtrl()
 {
+    BTN_CLICKS += 1;
     return BTN & (1 << BTN_CTRL);
 }
